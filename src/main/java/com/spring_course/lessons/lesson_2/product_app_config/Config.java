6382486e6ae251f7e87c43e473f6612e7dab6b64
0rque <marisergiv@gmail.com>
2021-04-12 13:16:19 +0300

    Solution for lesson 2

---
+++ src/main/java/com/spring_course/lessons/lesson_2/product_app_config/Config.java
@@ -0,0 +1,9 @@
+package com.spring_course.lessons.lesson_2.product_app_config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.spring_course.lessons.lesson_2")
+public class Config {
+}
